27bcc1e2f9273f8c9abdd20912346f0682ac31f7
Gia <giameimaranan25@gmail.com>
2025-09-21 18:03:13 +0800

    Add Docker setup, composer files, and update database connection

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'sql12799504',
+    'password'	=> 'zpqv8wnahf',
+    'database'	=> 'sql12799504',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
